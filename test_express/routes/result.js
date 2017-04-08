a3fbf956b6fdc9067e29c2aa991208fbf1e9122b
Sebas <seplarui@gmail.com>
2017-04-08 18:03:02 +0200

    Creada la ruta 'result' y muestra el resultado Console.log

---
+++ test_express/routes/result.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'seplarui',
+    database: 'demo'
+});
+
+router.get('/', function (req, rows, fields) {
+
+    connection.connect();
+
+    connection.query('select * from Tabla_1', function (err, rows, fields) {
+        console.log("El resultado es: ", rows[0].Nombre);
+        console.log("El resultado es: ", rows[1].Nombre);
+    })
+
+    connection.end();
+    
+});
+
+
+
+
+module.exports = router;
